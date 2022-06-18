0923265258f6b8a2be4ad80086d6fe777036d430
novando <rizkynovando@gmail.com>
2022-06-18 14:09:27 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","novando","kiki1995","techno");
+
+if(!$conn){
+	echo "gagal konek database menn";
+};
+
+?>
